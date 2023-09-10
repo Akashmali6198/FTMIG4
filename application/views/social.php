d5a6d68e4d7352b94957c280c968ac52b4ebdd65
akashmali6198 <maliakash6198@gmail.com>
2023-09-10 05:45:23 -0700

    social chang9

--- application/views/social.php
+++ application/views/social.php
@@ -139,7 +139,7 @@ padding: 0% 20% 3% 20%;
 
 .playlist-frame {
       width: 100%;
-      height: 200px;
+      height: 220px;
       border: solid white 1px;
     }
     .playlist-frame:hover{
@@ -285,10 +285,85 @@ padding: 0% 20% 3% 20%;
           <!-- youtube end -->
         </div>
      
-  <!-- tab4 -->
+  <!-- tab5 -->
     <div class="tab-pane fade" id="v-pills-4" role="tabpanel" aria-labelledby="v-pills-tab-4">
 
+<!-- twitter space start -->
+<section class="pt-120 pb-120 border-top-1">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-6 text-center">
+            <div class="section-header">
+              <h2 class="section-title"><span class="font-weight-normal">Our Latest </span> <b class="base--color">Spaces</b></h2>
+              <!-- <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse voluptatum eaque earum quos quia Id aspernatur ratione, voluptas nulla rerum laudantium neque ipsam eaque</p> -->
+            </div>
+          </div>
+        </div><!-- row end -->
+        <div class="row justify-content-center mb-none-30">
+          <div class="col-lg-4 col-md-6 mb-30">
+            <div class="blog-card">
+              <div class="blog-card__thumb">
+                <img class="imgsize" src="assets/images/space1.jpeg"  alt="image">
+              </div>
+              
+              <div class="blog-card__content">
+                <h4 class="blog-card__title mb-3"><a href="https://twitter.com/i/spaces/1vAxRAEmVXqJl">Join us Tuesday @10am ET as we learn all about @AIMLInnovations</a></h4>
+                <ul class="blog-card__meta d-flex flex-wrap mb-4">
+                  <li>
+                    <i class="las la-calendar"></i>
+                    <a href="https://twitter.com/i/spaces/1vAxRAEmVXqJl">18 April, 2023</a>
+                  </li>
+                </ul>
+                <p> $AIML AI/ML Innovations Inc is a company committed to acquiring and advancing Artificial Intelligence/Machine Learning technologies that address urgent societal needs</p>
+                <!-- <a href="#0" class="cmn-btn btn-md mt-4">Read More</a> -->
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 mb-30">
+            <div class="blog-card">
+              <div class="blog-card__thumb">
+                <img class="imgsize "src="assets/images/space2.jpeg" alt="image">
+              </div>
+              <div class="blog-card__content">
+                <h4 class="blog-card__title mb-3"><a href="https://twitter.com/i/spaces/1eaKbrWnVonKX">Join us live Thursday @8pm ET as we catch up with $FDR</a></h4>
+                <ul class="blog-card__meta d-flex flex-wrap mb-4">
+                <li>
+                    <i class="las la-calendar"></i>
+                    <a href="https://twitter.com/i/spaces/1eaKbrWnVonKX">31st August, 2023</a>
+                  </li>
+                </ul>
+                <p>Follow The Money Invest...@FoundersMetals President & CEO Colin Padget to discuss why his company is up 200% YOY in a down market
+<br><br></p>
+                <!-- <a href="#0" class="cmn-btn btn-md mt-4">Read More</a> -->
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 mb-30">
+            <div class="blog-card">
+              <div class="blog-card__thumb">
+                <img class="imgsize"src="assets/images/space3.jpeg" alt="image">
+              </div>
+              <div class="blog-card__content">
+                <h4 class="blog-card__title mb-3"><a href="https://twitter.com/i/spaces/1MYxNgqLOabKw">Join us Tuesday @8pm ET as we learn all about Green Transitions</a></h4>
+                <ul class="blog-card__meta d-flex flex-wrap mb-4">
+                  
+                  <li>
+                    <i class="las la-calendar"></i>
+                    <a href="https://twitter.com/i/spaces/1MYxNgqLOabKw">20 April, 2023</a>
+                  </li>
+                </ul>
+                <p>Follow The Money Inve... 19 Apr
+JOIN US TOMORROW AS WE DISCUSS WHY THE GREEN TRANSITION MEANS SCARCITY & OPPORTUNITY.
+</p>
+                <!-- <a href="#0" class="cmn-btn btn-md mt-4">Read More</a> -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
     
+<!-- twitter soace end -->
     </div>
 
 
