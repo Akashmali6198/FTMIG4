13c83ff6ddd9f418c2f801dabeab2de8767865e4
akashmali6198 <maliakash6198@gmail.com>
2023-09-11 06:05:22 -0700

    event page design updated

--- application/views/event.php
+++ application/views/event.php
@@ -1,28 +1,23 @@
 
-<style>
-  @media (max-width: 768px) {
 
-/* .card-medium-img.cmi-not-car {
+<style>
+
+@media (max-width: 768px) {
+
+.card-medium-img.cmi-not-car {
     padding: 14px!important;
-} */
+}
 .banner {
     height: 300px !important;
-    padding: 4%!important;
-
 }
-.banner h1 {
-    font-size: 39px!important;
 
   }
-  .row{
-    margin-right: unset;
-    margin-left: unset;
-}
-
-
-
-}
   /* media query */
+
+
+
+
+  /* banner style */
 
 .banner {
   position: relative;
@@ -32,7 +27,7 @@
   justify-content: flex-start;
   align-items: center;
   padding-left: 100px;
-  background: url('assets/images/bg/event_background.jpg') no-repeat center center;
+  background: url('assets/images/conference.jpg') no-repeat center center;
   background-size: cover;
 }
 
@@ -48,89 +43,371 @@
 </style>
 <section>
   <div class="container-fluid banner overlay">
-    <h1>Upcoming Events</h1>
+    <h1>Upcomming Events</h1>
   </div>
 
 </section>
- <!-- blog section start -->
- <section class="pt-120 pb-120 border-top-1">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-lg-6 text-center">
-            <div class="section-header">
-              <h2 class="section-title"><span class="font-weight-normal">Our Latest</span> <b class="base--color">Events</b></h2>
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse voluptatum eaque earum quos quia Id aspernatur ratione, voluptas nulla rerum laudantium neque ipsam eaque</p>
-            </div>
-          </div>
-        </div><!-- row end -->
-        <div class="row justify-content-center mb-none-30">
-          <div class="col-lg-4 col-md-6 mb-30">
-            <div class="blog-card">
-              <div class="blog-card__thumb">
-                <img src="assets/images/blog/1.jpg" alt="image">
+
+
+
+<!-- Events section start -->
+
+<!-- events section start -->
+<style>
+
+h2.sixip-latest-events {
+    font-family: Playfair Display;
+    font-style: normal;
+    font-weight: 900;
+    font-size: 36px;
+    line-height: 50px;
+    text-align: center;
+    color: #fff;
+    position: relative;
+}   .col-card-es {
+    padding: 10px;
+}.card-medium-single-event {
+    padding: 20px;
+    position: relative;
+}.card.event-card-medium.h-100 p.event-card-c-name {
+    height: 30px;
+    overflow: hidden;
+    font-size: 12px;
+    margin-bottom: 10px;
+}
+.card.event-card-medium.h-100 p.event-card-c-name {
+    height: 30px;
+    overflow: hidden;
+    font-size: 12px;
+    margin-bottom: 10px;
+}p.event-card-c-name:before {
+    position: absolute;
+    content: "";
+    left: 0;
+    top: 2px;
+    width: 12px;
+    height: 12px;
+    background: #78a343;
+}h4.card-medium-title {
+    height: 100px;
+    overflow: hidden;
+    margin-bottom: 30px;
+}
+h4.card-medium-title {
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 17px;
+    line-height: 26px;
+    color: #fff;
+}.card-medium-footers {
+    width: 100%;
+    display: flex;
+    align-items: center;
+}
+p.event-card-c-name {
+    font-family: Playfair Display;
+    font-style: normal;
+    font-weight: 900;
+    font-size: 14px;
+    line-height: 14px;
+    text-transform: uppercase;
+    color: #fff;
+    position: relative;
+    padding-left: 20px;
+    min-height: 15px;
+}.card-medium-footers p.event-date, .card-medium-footers p.event-time {
+    letter-spacing: .3px;
+    margin-bottom: 0;
+    font-size: 12px;
+}p.event-date {
+    padding: 7px;
+    background: #000;
+    width: 135px;
+    border: 1px solid #78a343;
+    max-height: 31px;
+color:white;
+font-weight:700;
+}
+.card-medium-footers i.far.fa-clock {
+    margin-right: 4px;
+    top: 3px;
+}.card-medium-footers p.event-time {
+    padding-left: 7px;
+    text-align: right;
+    bottom: 3px;
+    position: relative;
+    color:white;
+}img.las.la-calendar {
+    color: #78a343;
+}.latest-btn-holder {
+    margin: 45px auto 0 auto;
+    display: block;
+    text-align: center;
+}
+.sixip-btn-gold {
+    background: #000;
+    border: 1px solid #78a343;
+    border-radius: 4px;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 19px;
+    text-transform: uppercase;
+    color: #fff;
+    padding: 10px 25px;
+    transition: all .3s ease;
+}
+    </style>
+
+    <section class="sixip-home-events-wrap" style="padding:3%;">
+	<div class="container">
+
+		<div class="row">
+			<div class="col-sm-12 col-md-12">
+				<h2 class="sixip-latest-events">Our Latest Events
+        <hr style="border:solid 2px #78a343;width:15%">
+        </h2>
+			</div><!--end-col-->
+		</div><!--row-->
+
+ 
+        <div class="row sixip-home-events">
+	 
+                            
+							 
+							 
+ 
+
+							
+    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 col-card-es"> 
+        
+      <a href="index.php" rel="bookmark" title="SolGold Market Update and Live Q&#038;A">
+
+        <div class="card event-card-medium h-100">
+
+            
+              <div class="card-medium-img cmi-not-car"><iframe class="playlist-frame" src="https://www.youtube.com/embed/bTlDMnaWRhA" frameborder="0" allowfullscreen></iframe>
               </div>
-              <div class="blog-card__content">
-                <h4 class="blog-card__title mb-3"><a href="#0">Laboriosam distinctio nisi debitis deleniti voluptatum corporis.</a></h4>
-                <ul class="blog-card__meta d-flex flex-wrap mb-4">
-                  <li>
-                    <i class="las la-user"></i>
-                    <a href="#0">fahaddevs</a>
-                  </li>
-                  <li>
-                    <i class="las la-calendar"></i>
-                    <a href="#0">20 Nov, 2020</a>
-                  </li>
-                </ul>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisic elit. Iusto eos rem ducimus nam! Optio, soluta. Laboriosam distinctio nisi debitis deleniti ducim.</p>
-                <a href="#0" class="cmn-btn btn-md mt-4">Read More</a>
+
+            
+
+        <div class="card-medium-single-event cmse-not-car">
+
+           <p class="event-card-c-name">
+                                                            
+                   <span class="tsingle">SolGold </span> 
+
+                
+               
+                     </p> 
+
+   <div class="card-big-body"> 
+
+       
+          <h4 class="card-medium-title">SolGold Market Update and Live Q&#038;A</h4>
+
+            
+  </div><!--card-body-->
+
+  <div class="card-medium-footers">
+    <p class="event-date">
+        <img src="" class="las la-calendar">Jul 20, 2023</p>
+        <p class="event-time"><i class="far fa-clock"></i>8:00 am</p> 
+    </div><!--card-footer-->
+
+    
+</div><!--single-event-->
+
+</div><!--card-->
+</a>
+</div><!--end-col-->
+
+
+ 
+							 
+ 
+
+							   
+    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 col-card-es"> 
+        
+      <a href="index.php"  title="Chesapeake Gold Market Update + Live Q&#038;A">
+
+        <div class="card event-card-medium h-100">
+
+            
+              <div class="card-medium-img cmi-not-car">
+              <iframe class="playlist-frame" src="https://www.youtube.com/embed/7nHG0yZ7hj8" frameborder="0" allowfullscreen></iframe> 
               </div>
-            </div>
-          </div>
-          <div class="col-lg-4 col-md-6 mb-30">
-            <div class="blog-card">
-              <div class="blog-card__thumb">
-                <img src="assets/images/blog/2.jpg" alt="image">
+
+            
+
+        <div class="card-medium-single-event cmse-not-car">
+
+           <p class="event-card-c-name">
+                                                            
+                   <span class="tsingle">Chesapeake Gold </span> 
+
+                
+               
+                     </p> 
+
+   <div class="card-big-body"> 
+
+       
+          <h4 class="card-medium-title">Chesapeake Gold Market Update + Live Q&A</h4>
+
+            
+  </div><!--card-body-->
+
+  <div class="card-medium-footers">
+    <p class="event-date">
+    <img class="las la-calendar">Jul 10, 2023</p>
+        <p class="event-time"><i class="far fa-clock"></i>2:00 pm</p> 
+    </div><!--card-footer-->
+
+    
+</div><!--single-event-->
+
+</div><!--card-->
+</a>
+</div><!--end-col-->
+
+
+ 
+							 
+ 
+
+							   
+    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 col-card-es"> 
+        
+      <a href="" rel="bookmark" title="High-Interest Rates: Net-Good or a Detriment for Mining?">
+
+        <div class="card event-card-medium h-100">
+
+            
+              <div class="card-medium-img cmi-not-car">
+              <iframe class="playlist-frame" src="https://www.youtube.com/embed/1LVUGQaZg9M" frameborder="0" allowfullscreen></iframe> 
               </div>
-              <div class="blog-card__content">
-                <h4 class="blog-card__title mb-3"><a href="#0">Laboriosam distinctio nisi debitis deleniti voluptatum corporis.</a></h4>
-                <ul class="blog-card__meta d-flex flex-wrap mb-4">
-                  <li>
-                    <i class="las la-user"></i>
-                    <a href="#0">fahaddevs</a>
-                  </li>
-                  <li>
-                    <i class="las la-calendar"></i>
-                    <a href="#0">20 Nov, 2020</a>
-                  </li>
-                </ul>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisic elit. Iusto eos rem ducimus nam! Optio, soluta. Laboriosam distinctio nisi debitis deleniti ducim.</p>
-                <a href="#0" class="cmn-btn btn-md mt-4">Read More</a>
+
+            
+
+        <div class="card-medium-single-event cmse-not-car">
+
+           <p class="event-card-c-name">
+                                                            
+                   <span class="tsingle">Fireweed Metals 
+                
+                    Bunker Hill Mining , Galantas Gold </span>   
+                    
+                 
+               
+                             
+                    
+                 
+               
+                     </p> 
+
+   <div class="card-big-body"> 
+
+       
+          <h4 class="card-medium-title">High-Interest Rates: Net-Good or a Detriment for Mining?</h4>
+
+            
+  </div><!--card-body-->
+
+  <div class="card-medium-footers">
+    <p class="event-date">
+      <img class="las la-calendar">Jul 06, 2023</p>
+        <p class="event-time"><i class="far fa-clock"></i>10:00 am</p> 
+    </div><!--card-footer-->
+
+    
+</div><!--single-event-->
+
+</div><!--card-->
+</a>
+</div><!--end-col-->
+
+
+ 
+							 
+ 
+
+							
+    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 col-card-es"> 
+        
+      <a href="#" title="Economic Cooperation Strategy Briefing">
+
+        <div class="card event-card-medium h-100">
+
+            
+              <div class="card-medium-img cmi-not-car">
+              <iframe class="playlist-frame" src="https://www.youtube.com/embed/QV288nIoG24" frameborder="0" allowfullscreen></iframe> 
               </div>
-            </div>
-          </div>
-          <div class="col-lg-4 col-md-6 mb-30">
-            <div class="blog-card">
-              <div class="blog-card__thumb">
-                <img src="assets/images/blog/3.jpg" alt="image">
-              </div>
-              <div class="blog-card__content">
-                <h4 class="blog-card__title mb-3"><a href="#0">Laboriosam distinctio nisi debitis deleniti voluptatum corporis.</a></h4>
-                <ul class="blog-card__meta d-flex flex-wrap mb-4">
-                  <li>
-                    <i class="las la-user"></i>
-                    <a href="#0">fahaddevs</a>
-                  </li>
-                  <li>
-                    <i class="las la-calendar"></i>
-                    <a href="#0">20 Nov, 2020</a>
-                  </li>
-                </ul>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisic elit. Iusto eos rem ducimus nam! Optio, soluta. Laboriosam distinctio nisi debitis deleniti ducim.</p>
-                <a href="#0" class="cmn-btn btn-md mt-4">Read More</a>
-              </div>
-            </div>
-          </div>
-        </div>
+
+            
+
+        <div class="card-medium-single-event cmse-not-car">
+
+           <p class="event-card-c-name">
+                                                            
+                   <span class="tsingle">Canada-Africa Chamber of Business </span> 
+
+                
+               
+                     </p> 
+
+   <div class="card-big-body"> 
+
+       
+          <h4 class="card-medium-title">Economic Cooperation Strategy Briefing</h4>
+
+            
+  </div><!--card-body-->
+
+  <div class="card-medium-footers">
+    <p class="event-date">
+    <img src="" class="las la-calendar">Jul 05, 2023</p>
+        <p class="event-time"><i class="far fa-clock"></i>6:00 pm</p> 
+    </div><!--card-footer-->
+
+    
+</div><!--single-event-->
+
+</div><!--card-->
+</a>
+</div><!--end-col-->
+
+
+ 
+							
+					 
+        </div><!--end-row-->
+
+		<div class="row">
+      <div class="col-md-12">
+					<!-- <div class="latest-btn-holder">
+						<a href="event.php" class="sixip-btn-gold">View All On Demand <i class="fas fa-arrow-right"></i></a>
+          </div> -->
       </div>
-    </section>
-    <!-- blog section end -->
+		</div>
+           
+	</div><!--container-->
+
+
+
+    <!-- 2nd event row -->
+
+
+    <!--2nd event row end -->
+</section>  
+
+<!-- events section end -->
+
+
+
+<!-- 2nd events section start -->
+
+<!-- 2nd events end -->
